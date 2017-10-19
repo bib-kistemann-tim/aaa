ae387fe83eafd5e898d4599ec409686469c0be1c
KISTEMANN Tim <timkistemann@timkistemann@bib-buellingen.lokal>
2017-10-19 11:14:22 +0200

    Aufgabe 1

---
+++ ampel_TK.py
@@ -0,0 +1,16 @@
+class Ampel():
+	def __init__(self):
+		self.rot = rot
+		self.gruen = gruen
+		self.orange = orange
+		self.orangeblinken = orangeblinken
+	def setze():
+		self.farbe = farbe
+	def umschalten():
+		self.farbe = farbe + 1
+	def blinken():
+		self.orangeblinken
+		return blinken
+		self.rot
+	def Anzeigen():
+		
